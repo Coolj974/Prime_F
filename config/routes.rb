c4b3535e79cd816682e105e9037c3e8a118ad30c
rubyR <coolj974@hotmail.fr>
2017-07-17 06:31:06 -0700

    add Model Product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "team" => "pages#team"
+  get "contact" => "pages#join_us"
+  get "products" => "products#index"
+  get "products/:id" => "products#show"
+
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
